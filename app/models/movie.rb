fb2a206ef1d1a10ae98cc60ea85983eea496081c
Vinicius Rocha <vinicius.rocha@shopify.com>
2022-12-18 06:02:29 -0500

    Custom Queries

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,22 @@
 class Movie < ApplicationRecord
+  class << self
+    def released
+      where("released_on < ?", Time.now).order(released_on: :desc)
+    end
+
+    def hits
+      where("total_gross >= 300000000").order(total_gross: :desc)
+    end
+
+    def flops
+      where("total_gross < 22500000").order(total_gross: :asc)
+    end
+
+    def recently_added
+      order("created_at desc").limit(3)
+    end
+  end
+
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
