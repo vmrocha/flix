6d86052f45bfe9fd47dad9ed49243f5b323dbcd1
Vinicius Rocha <vinicius.rocha@shopify.com>
2022-12-27 07:26:00 -0300

    Handling Validation Errors

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,8 +8,12 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie = Movie.create(movie_params)
-    redirect_to movie
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -21,9 +25,12 @@ class MoviesController < ApplicationController
   end
 
   def update
-    movie = Movie.find(params[:id])
-    movie.update!(movie_params)
-    redirect_to movie
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to @movie
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
