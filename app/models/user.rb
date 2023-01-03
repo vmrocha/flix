7f9e9d8d9b27f52d914127faf59912ba57a159bd
Vinicius Rocha <vinicius.rocha@shopify.com>
2023-01-03 14:52:36 -0300

    User Signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
     format: { with: /\S+@\S+/ },
     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 10, allow_blank: true }
+
+  def gravatar_id
+    Digest::MD5::hexdigest(email.downcase)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :movies do
     resources :reviews
   end
+
   resources :users
+  get "signup" => "users#new"
 end
