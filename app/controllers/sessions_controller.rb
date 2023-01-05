4fe3fe387511cadbf196ab5889bb766af03040c8
Vinicius Rocha <vinicius.rocha@shopify.com>
2023-01-05 13:00:43 -0300

    Authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    fail
+    user = User.find_by(email: params[:email_or_username])
+    user = User.find_by(username: params[:email_or_username]) unless user
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = "Invalid email/username or password!"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
