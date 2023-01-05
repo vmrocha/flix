e81e4750358899795d2c0e7f5da0d3988c393dea
Vinicius Rocha <vinicius.rocha@shopify.com>
2023-01-05 12:34:28 -0300

    Sign In

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  resource :session, only: %i[new create destroy]
+  get "signin" => "sessions#new"
+
   resources :users
   get "signup" => "users#new"
 end
