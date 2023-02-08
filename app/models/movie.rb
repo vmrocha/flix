83de44f0aa53eaaf6160fe81be9126e6b8689f23
Vinicius Rocha <vmrocha@gmail.com>
2023-02-07 21:12:56 -0500

    Active Storage File Uploads: Part 2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,6 +16,7 @@ class Movie < ApplicationRecord
   validates :description, length: { minimum: 25 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
   validates :rating, inclusion: { in: RATINGS }
+  validate :acceptable_image
 
   scope :released,
         -> { where("released_on <= ?", Time.now).order(released_on: :desc) }
@@ -47,5 +48,18 @@ class Movie < ApplicationRecord
 
   def set_slug
     self.slug = title.parameterize if title
+  end
+
+  def acceptable_image
+    return unless main_image.attached?
+
+    unless main_image.blob.byte_size <= 1.megabyte
+      errors.add(:main_image, "is too big")
+    end
+
+    acceptable_types = %w[image/jpeg image/png]
+    unless acceptable_types.include?(main_image.content_type)
+      errors.add(:main_image, "must be a JPEG or PNG")
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -157,13 +157,34 @@ Movie.create!(
   ]
 )
 
-Genre.create!(name: "Action")
-Genre.create!(name: "Comedy")
-Genre.create!(name: "Drama")
-Genre.create!(name: "Romance")
-Genre.create!(name: "Thriller")
-Genre.create!(name: "Fantasy")
-Genre.create!(name: "Documentary")
-Genre.create!(name: "Adventure")
-Genre.create!(name: "Animation")
-Genre.create!(name: "Sci-Fi")
+%w[
+  Action
+  Comedy
+  Drama
+  Romance
+  Thriller
+  Fantasy
+  Documentary
+  Adventure
+  Animation
+  Sci-Fi
+].each { |name| Genre.create!(name: name) }
+
+[
+  ["Avengers: Endgame", "avengers-end-game.png"],
+  ["Captain Marvel", "captain-marvel.png"],
+  ["Black Panther", "black-panther.png"],
+  ["Avengers: Infinity War", "avengers-infinity-war.png"],
+  ["Green Lantern", "green-lantern.png"],
+  ["Fantastic Four", "fantastic-four.png"],
+  ["Iron Man", "ironman.png"],
+  %w[Superman superman.png],
+  %w[Spider-Man spiderman.png],
+  %w[Batman batman.png],
+  %w[Catwoman catwoman.png],
+  ["Wonder Woman", "wonder-woman.png"]
+].each do |movie_title, file_name|
+  movie = Movie.find_by!(title: movie_title)
+  file = File.open(Rails.root.join("app/assets/images/#{file_name}"))
+  movie.main_image.attach(io: file, filename: file_name)
+end
